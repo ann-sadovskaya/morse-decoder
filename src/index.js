0bb403c4004fb2e17dffed5861eb7edf9c9c318d
Hanna <hanna@Hannas-MacBook-Pro.local>
2020-03-22 16:27:22 +0300

    feat: add decision of morse decoder

--- src/index.js
+++ src/index.js
@@ -37,9 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const CHAR_TABLE = {
+    '10': '.',
+    '11': '-',
+    '00': ''
+};
+
+const SPACE = '**********';
+
 function decode(expr) {
-    // write your solution here
+    const charArr = expr.match(/.{1,10}/g);
+    let result = "";
+    for (let char of charArr) {
+        if (char === SPACE ) { 
+            result += " "
+        } else {
+            let morseArr = char.match(/.{1,2}/g)
+            let morseChar = ""
+            for (let morse of morseArr) {
+                morseChar += CHAR_TABLE[morse];
+            }
+            result += MORSE_TABLE[morseChar]
+        }
+    }
+    return result
 }
+
 
 module.exports = {
     decode
